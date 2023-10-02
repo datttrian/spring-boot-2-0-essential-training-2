51233394a58ca8cc3d7de962ed8efe68f41f1aff
datttrian <datttrian@gmail.com>
2023-10-02 23:07:35 +0000

    Solution: Generate an MVC webpage

--- room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/controllers/StaffController.java
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/controllers/StaffController.java
@@ -1,15 +1,15 @@
 package com.frankmoley.lil.sbet.landon.roomwebapp.controllers;
 
+import com.frankmoley.lil.sbet.landon.roomwebapp.service.StaffService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.frankmoley.lil.sbet.landon.roomwebapp.service.StaffService;
-
 @Controller
 @RequestMapping("/staff")
 public class StaffController {
+
     private final StaffService staffService;
 
     public StaffController(StaffService staffService) {
@@ -18,7 +18,7 @@ public class StaffController {
 
     @GetMapping
     public String getAllStaff(Model model) {
-        model.addAllAttributes("staff", staffService.getAllStaff());
+        model.addAttribute("staff", staffService.getAllStaff());
         return "staff";
     }
 
