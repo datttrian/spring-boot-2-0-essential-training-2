e6a832a2e099513f5a0ebfe71388f0a04b16c71a
datttrian <datttrian@gmail.com>
2023-10-02 22:44:04 +0000

    Solution: Generate an MVC webpage

--- room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/controllers/RoomController.java
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/controllers/RoomController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.frankmoley.lil.sbet.landon.roomwebapp.models.Room;
+import com.frankmoley.lil.sbet.landon.roomwebapp.service.RoomService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/rooms")
 public class RoomController {
 
-    private static final List<Room> rooms = new ArrayList<>();
-    static {
-        for(int i=0;i<10;i++){
-            rooms.add(new Room(i, "Room "+i, "R"+i, "Q"));
-        }
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
     }
 
     @GetMapping
-    public String getAllRooms(Model model){
-        model.addAttribute("rooms", rooms);
+    public String getAllRooms(Model model) {
+        model.addAttribute("rooms", roomService.getAllRooms());
         return "rooms";
     }
 }
