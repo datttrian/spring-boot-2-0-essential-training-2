f5a824e7243bd60bf9981cd8fffd6ba37e6c687a
datttrian <datttrian@gmail.com>
2023-10-03 23:16:58 +0000

    Solution: Build a CommandLineRunner

--- fizz-buzz/src/main/java/com/frankmoley/lil/sbet/clr/fizzbuzz/FizzBuzzApplication.java
+++ fizz-buzz/src/main/java/com/frankmoley/lil/sbet/clr/fizzbuzz/FizzBuzzApplication.java
@@ -1,5 +1,6 @@
 package com.frankmoley.lil.sbet.clr.fizzbuzz;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +11,15 @@ public class FizzBuzzApplication {
 		SpringApplication.run(FizzBuzzApplication.class, args);
 	}
 
+	public CommandLineRunner run() {
+		return args -> {
+			for (int i = 1; i < 101; i++) {
+				String result = "";
+				result += (i % 3) == 0 ? "Fizz" : "";
+				result += (i % 5) == 0 ? "Buzz" : "";
+				System.out.println(!result.isEmpty() ? result : i);
+				;
+			}
+		};
+	}
 }
