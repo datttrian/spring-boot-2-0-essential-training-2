34adf9620a3660425f4871a74d27f7a308555250
datttrian <datttrian@gmail.com>
2023-10-03 16:12:18 +0000

    Web services with Spring Boot

---
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/controllers/RoomRestController.java
@@ -0,0 +1,26 @@
+package com.frankmoley.lil.sbet.landon.roomwebapp.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.frankmoley.lil.sbet.landon.roomwebapp.models.Room;
+import com.frankmoley.lil.sbet.landon.roomwebapp.service.RoomService;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomRestController {
+    private final RoomService roomService;
+
+
+    public RoomRestController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+}
