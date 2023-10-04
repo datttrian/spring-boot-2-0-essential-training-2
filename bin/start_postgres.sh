78654bf0be68e04a8dddc4260c94f79a34acf352
datttrian <datttrian@gmail.com>
2023-10-04 16:30:22 +0000

    Connecting to external databases

--- bin/start_postgres.sh
+++ bin/start_postgres.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-which docker > /dev/null || (echoerr "Please ensure that docker is in your PATH" && exit 1)
+which docker > /dev/null || (echo "Please ensure that docker is in your PATH" && exit 1)
 
 mkdir -p $HOME/docker/volumes/postgres
 rm -rf $HOME/docker/volumes/postgres/data
