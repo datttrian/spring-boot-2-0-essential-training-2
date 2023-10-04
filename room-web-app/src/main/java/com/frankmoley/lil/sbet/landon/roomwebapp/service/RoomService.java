b5f9bfb6ddc098d93b9066e0a2e3be14ce8e8a41
datttrian <datttrian@gmail.com>
2023-10-04 15:57:14 +0000

    Building a Spring Data repository

--- room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/service/RoomService.java
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/service/RoomService.java
@@ -1,16 +1,21 @@
 package com.frankmoley.lil.sbet.landon.roomwebapp.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.frankmoley.lil.sbet.landon.roomwebapp.data.RoomRepository;
 import com.frankmoley.lil.sbet.landon.roomwebapp.models.Room;
 
 @Service
 public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
 
     public List<Room> getAllRooms() {
-        return rooms;
+        return roomRepository.findAll();
     }
 }
