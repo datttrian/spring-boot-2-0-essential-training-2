0ff318c19db0d33172e64a2e0f51a605380b54ac
datttrian <datttrian@gmail.com>
2023-10-04 15:50:05 +0000

    Building a Spring Data repository

---
+++ room-web-app/src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/data/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.frankmoley.lil.sbet.landon.roomwebapp.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class RoomRepository extends JpaRepository<Room, Long> {
+
+}
