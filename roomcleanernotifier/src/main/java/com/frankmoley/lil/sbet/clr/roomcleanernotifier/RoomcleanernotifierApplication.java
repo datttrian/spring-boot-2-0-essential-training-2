3e410117b7d69f10199b32bc624bf90bcdc803aa
datttrian <datttrian@gmail.com>
2023-10-04 23:03:04 +0000

    Building Spring message producers

---
+++ roomcleanernotifier/src/main/java/com/frankmoley/lil/sbet/clr/roomcleanernotifier/RoomcleanernotifierApplication.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.sbet.clr.roomcleanernotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomcleanernotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomcleanernotifierApplication.class, args);
+	}
+
+}
